e1a0b0e9d5ba51c80eb999f0922d98c54027a142
maurici0nunes <tecinfomauricio@gmail.com>
2019-07-10 15:31:59 -0300

    Aula 113 - Detalhes do Produto

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Projeto desenvolvido do zero no [Curso de PHP 7].
 
 Projeto utiliza: RainTPL, SlimFramework e PHPMailer.
+
+Template: https://www.free-css.com/free-css-templates/page201/digishop-mini
--- sql/bkp.sql
+++ sql/bkp.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `db_ecommerce` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `db_ecommerce`;
+-- MySQL dump 10.17  Distrib 10.3.16-MariaDB, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: db_ecommerce
+-- Host: localhost    Database: db_ecommerce
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.21-MariaDB
+-- Server version	10.3.16-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,7 +34,7 @@ CREATE TABLE `tb_addresses` (
   `descountry` varchar(32) NOT NULL,
   `deszipcode` char(8) NOT NULL,
   `desdistrict` varchar(32) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idaddress`),
   KEY `fk_addresses_persons_idx` (`idperson`),
   CONSTRAINT `fk_addresses_persons` FOREIGN KEY (`idperson`) REFERENCES `tb_persons` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -45,7 +47,6 @@ CREATE TABLE `tb_addresses` (
 
 LOCK TABLES `tb_addresses` WRITE;
 /*!40000 ALTER TABLE `tb_addresses` DISABLE KEYS */;
-INSERT INTO `tb_addresses` VALUES (1,7,'Avenida Brasil','de 26647 a 27767 - lado Ã­mpar','Rio de Janeiro','RJ','Brasil','21615338','Deodoro','2017-09-06 16:04:03'),(2,7,'Avenida Brasil','de 26647 a 27767 - lado ímpar','Rio de Janeiro','RJ','Brasil','21615338','Deodoro','2017-09-06 16:05:19'),(3,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 11:39:31'),(4,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 11:39:54'),(5,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 12:19:04'),(6,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 12:20:07'),(7,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 12:21:06'),(8,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 12:22:33'),(9,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-08 12:27:54'),(10,1,'Avenida Paulista','de 612 a 1510 - lado par','São Paulo','SP','Brasil','01310100','Bela Vista','2017-09-21 13:59:47');
 /*!40000 ALTER TABLE `tb_addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +64,7 @@ CREATE TABLE `tb_carts` (
   `deszipcode` char(8) DEFAULT NULL,
   `vlfreight` decimal(10,2) DEFAULT NULL,
   `nrdays` int(11) DEFAULT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idcart`),
   KEY `FK_carts_users_idx` (`iduser`),
   CONSTRAINT `fk_carts_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -76,7 +77,7 @@ CREATE TABLE `tb_carts` (
 
 LOCK TABLES `tb_carts` WRITE;
 /*!40000 ALTER TABLE `tb_carts` DISABLE KEYS */;
-INSERT INTO `tb_carts` VALUES (1,'8hcko3j7hmgp8sv7ggnseueupv',NULL,'22041080',NULL,2,'2017-09-04 18:50:50'),(2,'m8iq807es95o2hj1a30772df1d',NULL,'21615338',72.92,2,'2017-09-06 13:12:50'),(3,'cl1s6p6ti29vns970emmbhe7tb',7,NULL,NULL,NULL,'2017-09-06 13:46:15'),(4,'a8frnbabuqu60gguivlmrpagin',NULL,'01310100',61.12,1,'2017-09-08 11:39:01'),(5,'51jglmd9n3cdirc1ah75m31pt1',NULL,NULL,NULL,NULL,'2017-09-14 11:26:39'),(6,'tlvjs3tas1bml5uit8b5qgjn9l',NULL,'01310100',42.79,1,'2017-09-21 13:18:21');
+INSERT INTO `tb_carts` VALUES (1,'8hcko3j7hmgp8sv7ggnseueupv',NULL,'22041080',NULL,2,'2017-09-04 18:50:50'),(2,'m8iq807es95o2hj1a30772df1d',NULL,'21615338',72.92,2,'2017-09-06 13:12:50'),(4,'a8frnbabuqu60gguivlmrpagin',NULL,'01310100',61.12,1,'2017-09-08 11:39:01'),(5,'51jglmd9n3cdirc1ah75m31pt1',NULL,NULL,NULL,NULL,'2017-09-14 11:26:39'),(6,'tlvjs3tas1bml5uit8b5qgjn9l',NULL,'01310100',42.79,1,'2017-09-21 13:18:21');
 /*!40000 ALTER TABLE `tb_carts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +93,7 @@ CREATE TABLE `tb_cartsproducts` (
   `idcart` int(11) NOT NULL,
   `idproduct` int(11) NOT NULL,
   `dtremoved` datetime DEFAULT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idcartproduct`),
   KEY `FK_cartsproducts_carts_idx` (`idcart`),
   KEY `fk_cartsproducts_products_idx` (`idproduct`),
@@ -121,9 +122,9 @@ DROP TABLE IF EXISTS `tb_categories`;
 CREATE TABLE `tb_categories` (
   `idcategory` int(11) NOT NULL AUTO_INCREMENT,
   `descategory` varchar(32) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idcategory`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +133,7 @@ CREATE TABLE `tb_categories` (
 
 LOCK TABLES `tb_categories` WRITE;
 /*!40000 ALTER TABLE `tb_categories` DISABLE KEYS */;
-INSERT INTO `tb_categories` VALUES (1,'Android','2017-09-21 15:18:08'),(2,'IOS','2017-09-21 15:18:15'),(3,'Samsung','2017-09-21 15:19:14');
+INSERT INTO `tb_categories` VALUES (1,'Android','2017-09-21 15:18:08'),(3,'Samsung','2017-09-21 15:19:14'),(5,'Motorola','2019-07-10 12:42:40');
 /*!40000 ALTER TABLE `tb_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +151,7 @@ CREATE TABLE `tb_orders` (
   `idstatus` int(11) NOT NULL,
   `idaddress` int(11) NOT NULL,
   `vltotal` decimal(10,2) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idorder`),
   KEY `FK_orders_users_idx` (`iduser`),
   KEY `fk_orders_ordersstatus_idx` (`idstatus`),
@@ -169,7 +170,6 @@ CREATE TABLE `tb_orders` (
 
 LOCK TABLES `tb_orders` WRITE;
 /*!40000 ALTER TABLE `tb_orders` DISABLE KEYS */;
-INSERT INTO `tb_orders` VALUES (1,4,1,1,4,42.79,'2017-09-08 11:39:54'),(2,4,1,3,9,2359.12,'2017-09-08 12:27:54');
 /*!40000 ALTER TABLE `tb_orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,7 +183,7 @@ DROP TABLE IF EXISTS `tb_ordersstatus`;
 CREATE TABLE `tb_ordersstatus` (
   `idstatus` int(11) NOT NULL AUTO_INCREMENT,
   `desstatus` varchar(32) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idstatus`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -210,9 +210,9 @@ CREATE TABLE `tb_persons` (
   `desperson` varchar(64) NOT NULL,
   `desemail` varchar(128) DEFAULT NULL,
   `nrphone` bigint(20) DEFAULT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idperson`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,7 +221,7 @@ CREATE TABLE `tb_persons` (
 
 LOCK TABLES `tb_persons` WRITE;
 /*!40000 ALTER TABLE `tb_persons` DISABLE KEYS */;
-INSERT INTO `tb_persons` VALUES (1,'João Rangel','admin@hcode.com.br',2147483647,'2017-03-01 03:00:00'),(7,'Suporte','suporte@hcode.com.br',1112345678,'2017-03-15 16:10:27'),(8,'','',0,'2017-09-04 20:43:40'),(9,'Joao','joao@hcode.com.br',12345678,'2017-09-04 20:50:02'),(10,'Hcode','suporte@hcode.com.br',1112345678,'2017-09-06 14:32:15');
+INSERT INTO `tb_persons` VALUES (11,'admin','tecinfomauricio@gmail.com',0,'2019-07-09 16:30:28'),(12,'teste','tecinfomauricio@gmail.com',0,'2019-07-09 17:18:09');
 /*!40000 ALTER TABLE `tb_persons` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -241,7 +241,7 @@ CREATE TABLE `tb_products` (
   `vllength` decimal(10,2) NOT NULL,
   `vlweight` decimal(10,2) NOT NULL,
   `desurl` varchar(128) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idproduct`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -252,7 +252,7 @@ CREATE TABLE `tb_products` (
 
 LOCK TABLES `tb_products` WRITE;
 /*!40000 ALTER TABLE `tb_products` DISABLE KEYS */;
-INSERT INTO `tb_products` VALUES (1,'Smartphone Motorola Moto G5 Plus',1135.23,15.20,7.40,0.70,0.16,'smartphone-motorola-moto-g5-plus','2017-09-04 18:30:32'),(2,'Smartphone Moto Z Play',1887.78,14.10,0.90,1.16,0.13,'smartphone-moto-z-play','2017-09-04 18:30:32'),(3,'Smartphone Samsung Galaxy J5 Pro',1299.00,14.60,7.10,0.80,0.16,'smartphone-samsung-galaxy-j5','2017-09-04 18:30:32'),(4,'Smartphone Samsung Galaxy J7 Prime',1149.00,15.10,7.50,0.80,0.16,'smartphone-samsung-galaxy-j7','2017-09-04 18:30:32'),(5,'Smartphone Samsung Galaxy J3 Dual',679.90,14.20,7.10,0.70,0.14,'smartphone-samsung-galaxy-j3','2017-09-04 18:30:32');
+INSERT INTO `tb_products` VALUES (1,'Motorola Moto G5 Plus',1135.23,15.20,7.40,0.70,0.16,'smartphone-motorola-moto-g5-plus','2017-09-04 18:30:32'),(2,'Moto Z Play',1887.78,14.10,0.90,1.16,0.13,'smartphone-moto-z-play','2017-09-04 18:30:32'),(3,'Samsung Galaxy J5 Pro',1299.00,14.60,7.10,0.80,0.16,'smartphone-samsung-galaxy-j5','2017-09-04 18:30:32'),(4,'Samsung Galaxy J7 Prime',1149.00,15.10,7.50,0.80,0.16,'smartphone-samsung-galaxy-j7','2017-09-04 18:30:32'),(5,'Samsung Galaxy J3 Dual',679.90,14.20,7.10,0.70,0.14,'smartphone-samsung-galaxy-j3','2017-09-04 18:30:32');
 /*!40000 ALTER TABLE `tb_products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -279,6 +279,7 @@ CREATE TABLE `tb_productscategories` (
 
 LOCK TABLES `tb_productscategories` WRITE;
 /*!40000 ALTER TABLE `tb_productscategories` DISABLE KEYS */;
+INSERT INTO `tb_productscategories` VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(3,3),(3,4),(3,5),(5,1),(5,2);
 /*!40000 ALTER TABLE `tb_productscategories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -294,12 +295,12 @@ CREATE TABLE `tb_users` (
   `idperson` int(11) NOT NULL,
   `deslogin` varchar(64) NOT NULL,
   `despassword` varchar(256) NOT NULL,
-  `inadmin` tinyint(4) NOT NULL DEFAULT '0',
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `inadmin` tinyint(4) NOT NULL DEFAULT 0,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`iduser`),
   KEY `FK_users_persons_idx` (`idperson`),
   CONSTRAINT `fk_users_persons` FOREIGN KEY (`idperson`) REFERENCES `tb_persons` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -308,7 +309,7 @@ CREATE TABLE `tb_users` (
 
 LOCK TABLES `tb_users` WRITE;
 /*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
-INSERT INTO `tb_users` VALUES (1,1,'admin','$2y$12$kk3tykNHwIEWBPMuCSyvmubV2EhtQxNjT2HgXkYBLsPannGdy/KyC',1,'2017-03-13 03:00:00'),(7,7,'suporte','$2y$12$0HoqGJEa8UPrgQPYvDbjgOmnnnFXCVMehIRgM0zuPD87VM85Ykwjq',1,'2017-03-15 16:10:27'),(8,8,'','$2y$12$aEeuHG8QdmmcU1cZy.CLb.d8ffjQ4H0fGiNM.51ua9QYgYce.nVWG',0,'2017-09-04 20:43:40'),(9,9,'joao@hcode.com.br','$2y$12$Dz4iQ1CNYCGcOz9OVUoBXu7G7jnhfHcCFBj8l0kz7VPQcA2D.2Llu',0,'2017-09-04 20:50:02'),(10,10,'suporte@hcode.com.br','$2y$12$U1J8BTm4LHRVg8s9I9icneryymudMIdS51hBeIa0w60P8D4VRSs/m',1,'2017-09-06 14:32:15');
+INSERT INTO `tb_users` VALUES (11,11,'admin','$2y$12$kk3tykNHwIEWBPMuCSyvmubV2EhtQxNjT2HgXkYBLsPannGdy/KyC',1,'2019-07-09 16:30:28'),(12,12,'teste','$2y$12$bgQt/5tEYvEyUvCz5SIiOuIL8Qt87ezM9P.Tvt4/oixVAdXSq0oBK',1,'2019-07-09 17:18:09');
 /*!40000 ALTER TABLE `tb_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -327,7 +328,7 @@ CREATE TABLE `tb_userslogs` (
   `desuseragent` varchar(128) NOT NULL,
   `dessessionid` varchar(64) NOT NULL,
   `desurl` varchar(128) NOT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idlog`),
   KEY `fk_userslogs_users_idx` (`iduser`),
   CONSTRAINT `fk_userslogs_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -355,7 +356,7 @@ CREATE TABLE `tb_userspasswordsrecoveries` (
   `iduser` int(11) NOT NULL,
   `desip` varchar(45) NOT NULL,
   `dtrecovery` datetime DEFAULT NULL,
-  `dtregister` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dtregister` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`idrecovery`),
   KEY `fk_userspasswordsrecoveries_users_idx` (`iduser`),
   CONSTRAINT `fk_userspasswordsrecoveries_users` FOREIGN KEY (`iduser`) REFERENCES `tb_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -368,13 +369,8 @@ CREATE TABLE `tb_userspasswordsrecoveries` (
 
 LOCK TABLES `tb_userspasswordsrecoveries` WRITE;
 /*!40000 ALTER TABLE `tb_userspasswordsrecoveries` DISABLE KEYS */;
-INSERT INTO `tb_userspasswordsrecoveries` VALUES (1,7,'127.0.0.1',NULL,'2017-03-15 16:10:59'),(2,7,'127.0.0.1','2017-03-15 13:33:45','2017-03-15 16:11:18'),(3,7,'127.0.0.1','2017-03-15 13:37:35','2017-03-15 16:37:12'),(4,7,'127.0.0.1',NULL,'2017-09-06 13:44:34'),(5,7,'127.0.0.1','2017-09-06 10:45:54','2017-09-06 13:45:28');
 /*!40000 ALTER TABLE `tb_userspasswordsrecoveries` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'db_ecommerce'
---
 
 --
 -- Dumping routines for database 'db_ecommerce'
@@ -792,4 +788,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-29 11:35:12
+-- Dump completed on 2019-07-10 15:25:33
